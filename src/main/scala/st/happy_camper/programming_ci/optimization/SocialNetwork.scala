a9f90ddfe5aa3ebfb3bbdd8de4562526eaadf281
Takuya UESHIN <ueshin@happy-camper.st>
2011-10-09 23:16:30 +0900

    5.10.2 交差線のカウント
    Signed-off-by: Takuya UESHIN <ueshin@happy-camper.st>

--- src/main/scala/st/happy_camper/programming_ci/optimization/SocialNetwork.scala
+++ src/main/scala/st/happy_camper/programming_ci/optimization/SocialNetwork.scala
@@ -48,4 +48,35 @@ object SocialNetwork {
     (Joe -> Charlie),
     (Veruca -> Augustus),
     (Miranda, Joe))
+
+  /*
+   * 5.10.2 交差線のカウント
+   */
+  val domain = people.flatMap { _ => List((10, 370), (10, 370)) }
+
+  /**
+   * @param vec
+   * @return
+   */
+  def crossCount(vec: List[Int]): Double = {
+    assert(vec.size == people.size * 2)
+
+    val loc = (0 until people.size).map { i =>
+      people(i) -> (vec(2 * i), vec(2 * i + 1))
+    }.toMap
+
+    (for (i <- 0 until links.size; j <- i + 1 until links.size) yield {
+      val ((x1, y1), (x2, y2)) = (loc(links(i)._1), loc(links(i)._2))
+      val ((x3, y3), (x4, y4)) = (loc(links(j)._1), loc(links(j)._2))
+
+      val den = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
+      if (den == 0) {
+        0
+      } else {
+        val ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / den.toDouble
+        val ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / den.toDouble
+        if (ua > 0 && ua < 1 && ub > 0 && ub < 1) 1 else 0
+      }
+    }).sum
+  }
 }
